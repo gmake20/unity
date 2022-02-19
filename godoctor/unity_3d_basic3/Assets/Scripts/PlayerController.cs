0af8d7f6a164eb95e693898d92baabece138868c
andy <scym14@gmail.com>
2022-02-19 23:14:19 +0900

    고박사 : Unity 3D basic 3 3D Model & Animation

---
+++ godoctor/unity_3d_basic3/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private Animator animator;
+
+	private void Awake()
+	{
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		if ( Input.GetKeyDown(KeyCode.I) )
+		{
+			//animator.Play("Idle");
+			animator.SetFloat("moveSpeed", 0.0f);
+		}
+		else if ( Input.GetKeyDown(KeyCode.R) )
+		{
+			//animator.Play("RUN00_F");
+			animator.SetFloat("moveSpeed", 5.0f);
+		}
+	}
+}
+
