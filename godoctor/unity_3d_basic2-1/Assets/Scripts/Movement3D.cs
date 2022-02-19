4eb22892e388f86b998cc37eceebf74ac17a7366
andy <scym14@gmail.com>
2022-02-19 19:18:02 +0900

    고박사 : Unity 3D basic 2-1 Navigation Mesh 기초

---
+++ godoctor/unity_3d_basic2-1/Assets/Scripts/Movement3D.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement3D : MonoBehaviour
+{
+	[SerializeField]
+	private	float		 moveSpeed = 5.0f;
+	private	NavMeshAgent navMeshAgent;
+
+	private void Awake()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+
+	public void MoveTo(Vector3 goalPosition)
+	{
+		// 기존에 이동 행동을 하고 있었다면 코루틴 중지
+		StopCoroutine("OnMove");
+		// 이동 속도 설정
+		navMeshAgent.speed = moveSpeed;
+		// 목표지점 설정 (목표까지의 경로 계산 후 알아서 움직인다)
+		navMeshAgent.SetDestination(goalPosition);
+		// 이동 행동에 대한 코루틴 시작
+		StartCoroutine("OnMove");
+	}
+
+	IEnumerator OnMove()
+	{
+		while ( true )
+		{
+			// 목표 위치(navMeshAgent.destination)와 내 위치(transform.position)의 거리가 0.1미만일 때
+			// 즉, 목표 위치에 거의 도착했을 때
+			if ( Vector3.Distance(navMeshAgent.destination, transform.position) < 0.1f )
+			{
+				// 내 위치를 목표 위치로 설정
+				transform.position = navMeshAgent.destination;
+				// SetDestination()에 의해 설정된 경로를 초기화. 이동을 멈춘다
+				navMeshAgent.ResetPath();
+
+				break;
+			}
+
+			yield return null;
+		}
+	}
+}
+
---
+++ godoctor/unity_3d_basic2-1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+	private	Movement3D movement3D;
+
+	private void Awake()
+	{
+		movement3D = GetComponent<Movement3D>();
+	}
+
+	private void Update()
+	{
+		// 마우스 왼쪽 버튼을 눌렀을 때
+		if ( Input.GetMouseButtonDown(0) )
+		{
+			RaycastHit hit;
+			// Camera.main : 태그가 "Camera"인 오브젝트 = "Main Camera"
+			// 카메라로부터 마우스 좌표(Input.mousePosition) 위치를 관통하는 광선 생성
+			// ray.origin : 광선의 시작 위치
+			// ray.direction : 광선의 진행 방향
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			// Physics.Raycast() : 광선을 발사해서 부딪히는 오브젝트를 검출
+			// (광선에 부딪히는 오브젝트가 있으면 true 반환)
+			// ray.origin 위치로부터 ray.direction 방향으로 무한한 길이(Mathf.Infinity)의 광선 발사
+			// 광선에 부딪히는 오브젝트의 정보를 hit에 저장
+			if ( Physics.Raycast(ray, out hit, Mathf.Infinity) )
+			{
+				// hit.transform.position : 부딪힌 오브젝트의 위치
+				// hit.point : 광선과 오브젝트가 부딪힌 세부 위치
+				
+				// hit.point를 목표위치로 이동!
+				movement3D.MoveTo(hit.point);
+			}
+		}
+	}
+}
+
